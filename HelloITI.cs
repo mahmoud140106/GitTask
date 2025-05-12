8f25f30b8bb2f47d4719108d963e27de3d5862b6
mahmoud140106 <amer140106@gmail.com>
2025-05-12 13:41:31 +0300

    Initial commit with HelloWorld, HelloITI, and Test.txt

---
+++ HelloITI.cs
@@ -0,0 +1,12 @@
+import System;
+
+namespace Hello
+{
+    class HelloITI
+    {
+        public void printHelloITI()
+        {
+            System.out.println("Hello ITI");
+        }
+    }
+}
---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+import System;
+
+namespace Hello
+{
+    class HelloWorld
+    {
+        public static void PrintHelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
