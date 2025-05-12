66a3a491fcb2b92f710869964c319a6ff8e9fd28
mahmoud140106 <amer140106@gmail.com>
2025-05-12 13:43:29 +0300

    Added printHelloGit method to HelloITI

--- HelloITI.cs
+++ HelloITI.cs
@@ -8,5 +8,9 @@ namespace Hello
         {
             System.out.println("Hello ITI");
         }
+        public void printHelloGit()
+        {
+            System.out.println("Hello Git");
+        }
     }
 }
